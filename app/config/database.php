3633761915af5d44353445fc8ab4eb4a207949d5
tbredewold <thomas.bredewold@gmail.com>
2015-09-21 12:03:09 +0200

    checking another time for gitignore

--- app/config/database.php
+++ app/config/database.php
@@ -51,7 +51,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'laravel_blog',
 			'username'  => 'root',
-			'password'  => 'password',
+			'password'  => 'pass',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
